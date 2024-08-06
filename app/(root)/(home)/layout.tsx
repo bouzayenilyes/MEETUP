6622aa0af1cb17cabe2602062d67a4274c17d887
The Vimeleon <ilyesbouzayen@hotmail.com>
2024-08-06 21:27:37 +0100

    First Commit

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 
 export const metadata: Metadata = {
-  title: 'YOOM',
+  title: 'MEET UP',
   description: 'A workspace for your team, powered by Stream Chat and Clerk.',
 };
 
